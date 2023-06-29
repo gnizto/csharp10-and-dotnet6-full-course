402ec7a42f807a8d9340dbf06dc1f237956fe999
Gabriel DM <gnizto@outlook.com>
2023-06-29 20:53:33 -0300

    Return values

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,23 +1,32 @@
-﻿namespace HelloWorld
+﻿public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            WriteSomething();
-            WriteSomethingSpecific("I am an argument and am called from a method");
-            Console.Read();
-        }
+        Console.WriteLine(Add(15, 31));
 
-        public static void WriteSomething()
-        {
-            Console.WriteLine("I am called from a method");
-        }
+        int multiplyResult = Multiply(30, 3);
+        Console.WriteLine("The multiplication result is: " + multiplyResult);
 
-        public static void WriteSomethingSpecific(string myText)
-        {
-            Console.WriteLine(myText);
-        }
+        double divideResult = Divide(25, 13);
+        Console.WriteLine("The division result is: " + divideResult);
+
+        Console.Read();
+    }
+
+    public static int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public static int Multiply(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+
+    public static double Divide(double num1, double num2)
+    {
+        return num1 / num2;
     }
 }
+
 
