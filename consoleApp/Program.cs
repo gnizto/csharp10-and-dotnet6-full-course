deb6f0bda991ca447a4e463407922dab4054106d
Gabriel DM <gnizto@outlook.com>
2023-06-29 21:12:45 -0300

    Get user input

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -2,30 +2,28 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(Add(15, 31));
-
-        int multiplyResult = Multiply(30, 3);
-        Console.WriteLine("The multiplication result is: " + multiplyResult);
-
-        double divideResult = Divide(25, 13);
-        Console.WriteLine("The division result is: " + divideResult);
-
+        Calculate();
         Console.Read();
     }
 
-    public static int Add(int num1, int num2)
+    private static void Calculate()
+    {
+        Console.WriteLine("/// Let's sum two numbers ///");
+
+        Console.Write("Enter the first one: ");
+        double num1 = Double.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second one: ");
+        double num2 = Double.Parse(Console.ReadLine());
+
+        double result = Add(num1, num2);
+
+        Console.WriteLine($"The result is {result}");
+    }
+
+    private static double Add(double num1, double num2)
     {
         return num1 + num2;
-    }
-
-    public static int Multiply(int num1, int num2)
-    {
-        return num1 * num2;
-    }
-
-    public static double Divide(double num1, double num2)
-    {
-        return num1 / num2;
     }
 }
 
