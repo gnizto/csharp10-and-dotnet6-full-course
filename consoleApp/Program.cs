6004da3ade71452fc1182fdf7b5203fd6d26e51a
Gabriel DM <gnizto@outlook.com>
2023-07-01 23:12:05 -0300

    Ternary challenge solution

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -9,11 +9,11 @@
 
     private static void StateOfMatter()
     {
-        int temperature = -5;
+        int temperature = 5;
         string stateOfMatter;
 
-        // Challenge 
-        stateOfMatter = temperature < 0 ? "Solid" : "Liquid";
+        // Challenge -> Add the gas state of matter to the options
+        stateOfMatter = temperature < 0 ? "Solid" : temperature > 100 ? "Gas" : "Liquid";
 
         Console.WriteLine("State of Matter is {0}", stateOfMatter);
     }
