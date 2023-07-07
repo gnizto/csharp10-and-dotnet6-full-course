7a2b403315a08120dc3a302f63ff5c79bdaf8384
Gabriel DM <gnizto@outlook.com>
2023-07-06 23:14:02 -0300

    Using arrays

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -6,8 +6,19 @@ namespace consoleApp
     {
         static void Main(string[] args)
         {
-            Members member1 = new Members();
-            member1.Introducing(true);
+            int[] grades = new int[5];
+
+            grades[0] = 20;
+            grades[1] = 15;
+            grades[2] = 12;
+            grades[3] = 9;
+            grades[4] = 7;
+
+            Console.Write("Enter some note: ");
+            string input = Console.ReadLine();
+            grades[0] = int.Parse(input);
+
+            Console.WriteLine("Grades at index 0: {0}", grades[0]);
         }
 
 
