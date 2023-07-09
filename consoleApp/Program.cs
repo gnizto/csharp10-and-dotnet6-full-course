4fbe56ab4884b24cd5ae8a2c43d94a9dac6eac00
Gabriel DM <gnizto@outlook.com>
2023-07-08 23:47:28 -0300

    Non challenge related code removed

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -6,26 +6,6 @@ namespace consoleApp
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[10];
-
-            for(int i = 0; i < nums.Length; i++)
-            {
-                nums[i] = i;
-            }
-
-            for(int j = 0; j < nums.Length; j++)
-            {
-                Console.WriteLine("Element at position {0} = {1}", j, nums[j]);
-            }
-
-            // Using foreach
-            int position = 0;
-            foreach(int num in nums)
-            {
-                Console.WriteLine("Element at position {0} = {1}", position, num);
-                position++;
-            }
-
             Console.WriteLine("---------Challenge---------");
             // Challenge: create an array with 5 names
             // Use a foreach to greet them all
